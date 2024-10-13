2dd252ef35e03e23f269c787c3b665ad5d6326a6
Gomila <lautaromgomila@gmail.com>
2024-10-13 12:30:47 -0300

    Ver_Guardias accesible
    ahora la pantalla Ver_guardias es accesible

--- Programa_Club/src/Modulo_Seguridad/Pantalla_Seguridad.java
+++ Programa_Club/src/Modulo_Seguridad/Pantalla_Seguridad.java
@@ -6,11 +6,13 @@ vM=La pantalla del Main del paquete programa_club
 package Modulo_Seguridad;
 
 import programa_club.Main;
+import Modulo_Seguridad.Ver_Guardias;
 
 public class Pantalla_Seguridad extends javax.swing.JFrame {
 
     public Pantalla_Seguridad() {
         initComponents();
+        this.setResizable(false);
     }
 
     @SuppressWarnings("unchecked")
@@ -18,27 +20,32 @@ public class Pantalla_Seguridad extends javax.swing.JFrame {
     private void initComponents() {
 
         label1 = new java.awt.Label();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btn_Volver = new javax.swing.JButton();
+        btn_verGuardias = new javax.swing.JButton();
+        btn_suspenderSocios = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         label1.setText("Seccion de Seguridad");
 
-        jButton1.setText("<--");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_Volver.setText("<--");
+        btn_Volver.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_VolverActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Ver Los Guardias");
-
-        jButton3.setText("Suspender Socio");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        btn_verGuardias.setText("Ver Los Guardias");
+        btn_verGuardias.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                btn_verGuardiasActionPerformed(evt);
+            }
+        });
+
+        btn_suspenderSocios.setText("Suspender Socio");
+        btn_suspenderSocios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_suspenderSociosActionPerformed(evt);
             }
         });
 
@@ -49,14 +56,14 @@ public class Pantalla_Seguridad extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_Volver, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(66, 66, 66)
                         .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jButton2)
+                        .addComponent(btn_verGuardias)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton3)))
+                        .addComponent(btn_suspenderSocios)))
                 .addContainerGap(112, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -66,28 +73,37 @@ public class Pantalla_Seguridad extends javax.swing.JFrame {
                 .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(262, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jButton1)
+                .addComponent(btn_Volver)
                 .addGap(80, 80, 80)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
+                    .addComponent(btn_verGuardias)
+                    .addComponent(btn_suspenderSocios))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_VolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_VolverActionPerformed
         Main vM = new Main();
         vM.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        vM.setSize(1530,900);
+    }//GEN-LAST:event_btn_VolverActionPerformed
+    
+    private void btn_suspenderSociosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_suspenderSociosActionPerformed
         Suspender_socio_interface vS= new Suspender_socio_interface();
         vS.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_btn_suspenderSociosActionPerformed
+
+    private void btn_verGuardiasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_verGuardiasActionPerformed
+        Ver_Guardias VerGuardias=new Ver_Guardias();
+        VerGuardias.setVisible(true);
+        VerGuardias.setLocationRelativeTo(null);
+        this.setVisible(false);
+        VerGuardias.setSize(1530,900);
+    }//GEN-LAST:event_btn_verGuardiasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,9 +140,10 @@ public class Pantalla_Seguridad extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton btn_Volver;
+    private javax.swing.JButton btn_suspenderSocios;
+    private javax.swing.JButton btn_verGuardias;
     private java.awt.Label label1;
     // End of variables declaration//GEN-END:variables
-}
+}                                          
+  
